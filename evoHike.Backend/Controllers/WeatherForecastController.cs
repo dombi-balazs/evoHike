05a994ff389c1fb5ffee8574f4dd0e67be37c293
vigeomart <veresmartin2006@gmail.com>
2025-11-16 21:56:53 +0100

    Mégis csak működik

--- evoHike.Backend/Controllers/WeatherForecastController.cs
+++ evoHike.Backend/Controllers/WeatherForecastController.cs
@@ -2,23 +2,29 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace evoHike.Backend.Controllers;
-
+/// <summary>
+/// Időjárás előrejelzés kontroller
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+  
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
-
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
     }
-
+    /// <summary>
+    /// Megadja az időjárás előrejelést az elkövetkező 5 napba
+    /// </summary>
+    /// <returns>Időjárás előrejelzés adatok</returns>
+    /// <response code="200">Visszadja az időjárás előrejelzést</response>
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
